f0db4dbf14931bc901d21cbc34d07834126a8411
Calvin Donner <calvinjdonner@gmail.com>
2021-12-18 17:58:43 -0700

    22.1.6 end

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,23 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useQuery } from '@apollo/client';
 
 import ProductItem from '../ProductItem';
 import { QUERY_PRODUCTS } from '../../utils/queries';
 import spinner from '../../assets/spinner.gif';
+import { useStoreContext } from '../../utils/GlobalState';
+import { UPDATE_PRODUCTS } from '../../utils/actions';
 
-function ProductList({ currentCategory }) {
+function ProductList() {
+  const [state, dispatch] = useStoreContext();
+
+  const { ProductList } = state;
+  
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-
-  const products = data?.products || [];
-
-  function filterProducts() {
-    if (!currentCategory) {
-      return products;
+  
+  useEffect(() => {
+    if (data) {
+      dispatch({
+        type: UPDATE_PRODUCTS,
+        products: data.products
+      });
     }
-
-    return products.filter(
-      (product) => product.category._id === currentCategory
-    );
+  }, [data, dispatch]);
+  
+  function filterProducts() {
+    if (!ProductList) {
+      return state.products;
+    }
+  
+    return state.products.filter(product => product.category._id === ProductList);
   }
 
   return (
